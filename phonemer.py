160447450f49b667676fcb94334add7fc5fcce27
therewasaguy <jason.sigal@gmail.com>
2016-01-30 19:42:11 -0500

    working with one line

--- phonemer.py
+++ phonemer.py
@@ -1,4 +1,6 @@
 import sys, io
+import markov
+import pickle
 
 linecount = 0;
 
@@ -8,6 +10,12 @@ file_name = sys.argv[1]
 phoneme_dict = {}
 syllable_dict = {}
 vowel_dict = {}
+
+first_word = 'It'
+prev_word = 'It'
+
+# n-gram size for markov
+n = 1
 
 # return array where
 # [0] is string of 0 1 2 corresponding to the stress of the vowel phonemes
@@ -54,20 +62,32 @@ for line in open(file_name, 'r'):
 			# filter out non alpha but leave apostrophes
 			if not char.isalpha() and char is not "'":
 				word = word.replace(char, "")
-		song_syls += str(syllable_dict[word])
-	song_syls += '\n'
-#	print song_syls
+		try:
+			song_syls += str(syllable_dict[word])
+			# break
+		except:
+			print 'no word: ' + word
 
-# print song_syls
+	song_syls += '\n'
+
+print song_syls
 
 
 # print phoneme_dict[test_word]
 # print syllable_dict[test_word]
 # print vowel_dict[test_word]
+# print 'it is ' + str(syllable_dict["IT"])
 
 
-def writenewline(current_line):
-	newsong_line = ""
+with open('yelp_markov_small.pickle', 'rb') as handle:
+	model = pickle.load(handle)
+
+def next_word(word):
+	return markov.generate(model, n, seed=word, max_iterations=2)
+
+
+def writenewline(current_line, prev_word):
+	newsong_line = prev_word + ' '
 
 	# for each line create a string current_line of syllables in the input song
 	# and a string newsong_line of new lyrics
@@ -75,33 +95,46 @@ def writenewline(current_line):
 
 
 	c = len(current_line)
-#	limit = 0
-#	while (c > 0) and (limit < 5):			#for testing
-	while c > 0: 
-		# use markov function to choose a next word
-		nextword = markovchainword()
-#		nextword = "SYLVIA"			#for testing	
+	#print "len current line: " + str(c)
 
-		# get syllable string for next word
-		nextword_syl = syllable_dict[nextword]  
-		print nextword_syl
+	# limit = 0
+	# while (c > 0) and (limit < 5):			#for testing
+	while c > 0: 
+
+		# use markov function to choose a next word
+		nextword = next_word(prev_word.upper())[1]
+		try: 
+			# get syllable string for next word
+			nextword_syl = syllable_dict[nextword]
+		except:
+			break
 		n = len(nextword_syl)
 		# if the next word chosen by the markov chain has the correct
 		# syllable count, it will be added to the new song lyrics
 		# otherwise a new word will be chosen 
 		if c > n:
-			for i in range(0,n-1):
+			for i in range(n-1):
 				if current_line[i] != nextword_syl[i]:
-					break
-				else:	
-					current_line = current_line[n:c]
-					newsong_line += nextword
-	
-		limit += 1
+					break	
+			current_line = current_line[n:]
+			newsong_line = newsong_line + nextword + ' '
+			c = len(current_line)
+		else:
+			break
+		# limit += 1
+	#	print current_line
 #	return newsong_line 
-	print newsong_line
+	print 'new song line: ' + newsong_line
 
-writenewline("10010001010100")
+writenewline("110010001010100", "THEY")
+
+# prev_word = "it"
+# nextword = next_word(prev_word.upper())[1]
+# print nextword
+# print syllable_dict[nextword]
+# print next_word(prev_word.upper())
+
+
 
 # TO DO: create signature for lyrics
 # for line in open(file_name, 'r'):
